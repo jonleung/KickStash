4050ff52c00b83e3b386af68c723c8cf8af60edd
Jonathan Leung <jon@j.local>
2013-11-11 12:29:30 -0500

    FINALLY DONE AD CAMPGIN

--- code.rb
+++ code.rb
@@ -6,6 +6,7 @@ Let me add some ad stuff
 Finish adding ad stuff
 Just kidding didn't finsh adding ad stuff I'm still workin on it
 I am still working on it
+Finally, I finish ad campaign stuff
 
 
 class Code
